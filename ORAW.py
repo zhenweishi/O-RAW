4a26926805ae7c3e788dd7009caf6fda670c0afe
Zhenwei Shi <zhenweishi88@gmail.com>
2018-06-19 21:17:56 +0200

    allow a specific ROI

--- ORAW.py
+++ ORAW.py
@@ -139,11 +139,9 @@ def executeORAWbatch_all(ptid,roi,myStructUID,exportDir,export_format,export_nam
 
   #----------------------------For a Specific ROI-----------------
 
-'''
-def executeORAWbatch_roi(myWorkingDirectory,roi,exportDir,export_format,export_name):
+def executeORAWbatch_roi(ptid,roi,myStructUID,exportDir,export_format,export_name,Img_path,RT_path,excludeStructRegex):
   outPath = r''
-  #read Params file of Pyradiomics
-  progress_filename = os.path.join(outPath, 'pyrexbatch_log.txt')
+  progress_filename = os.path.join(outPath, 'O-RAW_log.txt')
   # Configure logging
   rLogger = logging.getLogger('radiomics')
 
@@ -161,24 +159,26 @@ def executeORAWbatch_roi(myWorkingDirectory,roi,exportDir,export_format,export_n
   logger.info('Reading Params file for pyradiomics')
   # Reading Params file for pyradiomics
   try:
-  	paramsFile = os.path.join(os.getcwd(),'ParamsSettings','Pyradiomics_Params.yaml')
+    paramsFile = os.path.join(os.getcwd(),'ParamsSettings','Pyradiomics_Params.yaml')
   except Exception:
     logger.error('Could not find params file of Pyradiomics!', exc_info=True)
     exit(-1)
 #  logger.info('Reading Params file of Pyrex')
-
-  patient = os.listdir(myWorkingDirectory)
-#  logger.info('Parsing DICOM files and RTSTRUCT in working directory')
-  #xnat_collection(myWorkingDirectory,collectionURL,myProjectID)
-  Img_path,RT_path = ParseStructure(myWorkingDirectory) #detect the path of Image and RTstruct
+  patient = ptid
+  logger.info('Parsing DICOM files and RTSTRUCT in working directory')
   logger.info('DICOM and RTSTRUCT Parsing Done')
 
   flists = pandas.DataFrame(data= {'patient':patient}).T # create a pandas data frame for data
   logger.info('Starting Pyrex')
 
-  frames = pandas.DataFrame()
+  # define export output format
+  if export_format == 'csv':
+    RESULT = pandas.DataFrame()
+  else:
+    RESULT = Graph()
+
   for entry in flists:
-      results = pandas.DataFrame()
+      #results = pandas.DataFrame()
 #      logger.info('processing patient: %s', patient[entry])
       mask_vol=PyrexReader.Read_RTSTRUCT(RT_path[entry])
       logger.info('Loading RTSTRUCT: %s', RT_path[entry])
@@ -187,47 +187,50 @@ def executeORAWbatch_roi(myWorkingDirectory,roi,exportDir,export_format,export_n
       for j in range(0,len(M.StructureSetROISequence)):
           target.append(M.StructureSetROISequence[j].ROIName)
       logger.info('ROI: %s', target)
-      
-      if roi in target:
+      for k in range(0,len(target)):
+          if not re.search(roi,target[k]):
+              print('skip ROI: %s' % target[k])
+              continue
           try:
               featureVector = flists[entry]
-              #featureVector['patient'] = featureVector['patient'] + 'Pyrex_' + target[k]
-              Image,Mask = PyrexReader.Img_Bimask(Img_path[entry],RT_path[entry],roi)
-              logger.info('Processing Radiomics on %s of Patient (%s)',roi,patient[entry])
-              if export_format == 'csv': # sava results in csv
+              Image,Mask = PyrexReader.Img_Bimask(Img_path[entry],RT_path[entry],target[k])
+              logger.info('Processing Radiomics on %s of Patient (%s)',target[k],patient[entry])
+              if export_format == 'csv': # save results in csv
                   try:
                       result = pandas.Series(PyrexWithParams.CalculationRun(Image,Mask,paramsFile))
-                      contour = pandas.Series({'contour':roi})
+                      contour = pandas.Series({'contour':target[k]})
+                      structUID = pandas.Series({'structUID':myStructUID})
                       featureVector = featureVector.append(contour)
+                      featureVector = featureVector.append(structUID)
                       featureVector = featureVector.append(result)
-                      featureVector.name = 1
+                      featureVector.name = k
+                      results = pandas.DataFrame()
                       results = results.join(featureVector, how='outer')
                       Image = []
                       Mask= []
                       result=[]
-                      frames = pandas.concat([frames,results],axis=1)
+                      RESULT = pandas.concat([RESULT,results],axis=1)
                   except Exception:
                     logger.error('FEATURE EXTRACTION FAILED for CSV output:', exc_info=True)
-              else:# save results in triple stroe
+              else:# save results in triple store
                   try:
                     featureVector = PyrexWithParams.CalculationRun(Image,Mask,paramsFile) #compute radiomics
-                    featureVector.update({'patient':patient[entry],'contour':roi}) #add patient ID and contour                
-                    PyrexOutput.RadiomicsRDF(featureVector,exportDir,patient[entry],roi,export_format,export_name) #store radiomics locally with a specific format 
+                    featureVector.update({'patient':patient[entry],'contour':target[k],'RTid':myStructUID}) #add patient ID and contour                
+                    graph_roi = PyrexOutput.RadiomicsRDF(featureVector,exportDir,patient[entry],myStructUID,target[k],export_format,export_name) #store radiomics locally with a specific format 
+                    RESULT = RESULT + graph_roi
                     logger.info('Extraction complete, writing rdf')
                   except Exception:
                     logger.error('FEATURE EXTRACTION FAILED for RDF output:', exc_info=True)
           except Exception:
               logger.info('FEATURE EXTRACTION FAILED:')
           logger.info('-------------------------------------------')
-          print(patient[entry],roi)
+          print(patient[entry],target[k])
+  # return RESULT
+      if export_format == 'csv':
+          logger.info('Extraction complete, writing CSV')          
+          outputFilepath = os.path.join(exportDir,export_name+patient[entry]+ myStructUID+'.csv')     
+          RESULT.T.to_csv(outputFilepath, index=False, na_rep='NaN')
+          logger.info('CSV writing complete')
+          logger.info('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
       else:
-          print('%s is not exist in RTSTRUCTURE', roi)
-
-  logger.info('Extraction complete, writing CSV')          
-  outputFilepath = os.path.join(exportDir,export_name+'.csv')
-  #fullresults = pandas.concat(frame)
-#      outputFilepath = os.path.join(exportDir,'testpatient'+'.csv')      
-  frames.T.to_csv(outputFilepath, index=False, na_rep='NaN')
-  logger.info('CSV writing complete')
-  logger.info('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
-'''
+        RESULT.serialize(exportDir + os.sep + patient[entry]+ '_' + myStructUID +".ttl", format='turtle')
